2d685c40514ec02c24f16e5173738d7172b07251
Kaja Więckowska <k.wieckowska66@gmail.com>
2020-08-17 23:05:29 +0200

    Added script to sound button functionality

--- Assets/Scripts/SoundsPlayer.cs
+++ Assets/Scripts/SoundsPlayer.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundsPlayer : MonoBehaviour
 {
-    
+    [SerializeField] private List<AudioClip> _audioClips;
+    [SerializeField] private AudioSource _audioSource;
+
+    public void PlayRandomSound()
+    {
+        _audioSource.clip = _audioClips[Random.Range(0, _audioClips.Count)];
+        _audioSource.Play();
+    }
+
+    public void ChangeAmountOfSounds(Toggle toggle, AudioClip audioClip)
+    {
+        if(toggle.isOn)
+        {
+            _audioClips.Add(audioClip);
+        }
+        else
+        {
+            _audioClips.Remove(audioClip);
+        }
+    }
 }
