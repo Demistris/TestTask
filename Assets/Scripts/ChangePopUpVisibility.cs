77b2733e5f95ddb66fa74bcf631b70417c5a4748
Kaja Więckowska <k.wieckowska66@gmail.com>
2020-08-17 22:15:15 +0200

    Created ButtonsManager and added functionality to OpenPopUpButton and DisplayTextButton

---
+++ Assets/Scripts/ChangePopUpVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePopUpVisibility : MonoBehaviour
+{
+    [SerializeField] private Animator _popUp;
+
+    public void OpenPopUp()
+    {
+        _popUp.SetBool("isOpen", true);
+    }
+
+    public void ClosePopUp()
+    {
+        _popUp.SetBool("isOpen", false);
+    }
+}
---
+++ Assets/Scripts/TextDisplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDisplayer : MonoBehaviour
+{
+    [SerializeField] private GameObject _textToDisplay;
+    private float _timeToWait = 3f;
+
+    public void DisplayTexts()
+    {
+        _textToDisplay.SetActive(true);
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(_timeToWait);
+        _textToDisplay.SetActive(false);
+    }
+}
